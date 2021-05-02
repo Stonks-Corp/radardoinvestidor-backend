d21b973206d190149f62532f22ba63af3f4b161e
Pércio Reinert <percioreinert@gmail.com>
2021-05-02 19:41:22 -0300

    leitura do arquivo json nao funcional

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,43 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { fundos } from './fundosMock.json'
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.fundo.upsert({
-    where: {cnpj_fundo: 'line[0]'},
-    update: {},  
-    create: {
-        cnpj_fundo: 'line[0]',
-        dt_cancel: new Date('29/04/2021'),
-        admin: 'ADMIN',
-        cd_cvm: 'CD_CVM',
-        dt_fim_exerc: new Date('30/04/2021'),
-        classe: 'CLASSE',
-        dt_ini_ativ: new Date('28/04/2021'),
-        cnpj_admin: 'CNPJ_ADMIN',
-        dt_ini_classe: new Date('01/05/2021'),
-        dt_ini_exerc: new Date('02/05/2021'),
-        condom: 'CONDOM',
-        dt_ini_sit: new Date('03/05/2021'),
-        cpf_cnpj_gestor: 'CPF_CNPJ_GESTOR',
-        sit: 'SIT',
-        rentab_fundo: 'RENTAB_FUNDO',
-        pf_pj_gestor: 'PF_PJ_GESTOR',
-        invest_qualif: 'INVEST_QUALIF',
-        vl_patrim_liq: String(0),
-        gestor: 'GESTOR',
-        trib_lprazo: 'TRIB_LPRAZO',
-        fundo_exclusivo: 'FUNDO_EXCLUSIVO',
-        tp_fundo: 'TP_FUNDO',
-        fundo_cotas: 'FUNDO_COTAS',
-        taxa_perfm: String(0),
-        dt_patrim_liq: new Date('04/05/2021'),
-        taxa_adm: String(0),
-        auditor: 'AUDITOR',
-        denom_social: 'DENOM_SOCIAL',
-        cnpj_auditor: 'CNPJ_AUDITOR',
-        dt_const: new Date('05/05/2021'),
-      },
+
+  const fundosKeys = Object.keys(fundos)
+  const data = []
+  fundosKeys.forEach( (key) => {
+    data.push(fundos[key])
   })
+  
 }
 
 main()
--- src/controllers/interface.ts
+++ src/controllers/interface.ts
@@ -1,35 +1,37 @@
+export interface IFundAttributes {
+  SIT: string;
+  ADMIN: string;
+  CD_CVM: string;
+  CLASSE: string;
+  CNPJ_ADMIN: string;
+  CONDOM: string;
+  CPF_CNPJ_GESTOR: string;
+  DENOM_SOCIAL: string;
+  DT_CANCEL: string;
+  DT_CONST: string;
+  DT_FIM_EXERC: string;
+  DT_INI_ATIV: string;
+  DT_INI_CLASSE: string;
+  DT_INI_EXERC: string;
+  DT_INI_SIT: string;
+  DT_PATRIM_LIQ: string;
+  FUNDO_COTAS: string;
+  FUNDO_EXCLUSIVO: string;
+  GESTOR: string;
+  INVEST_QUALIF: string;
+  PF_PJ_GESTOR: string;
+  RENTAB_FUNDO: string;
+  TAXA_ADM: number;
+  TAXA_PERFM: number;
+  TP_FUNDO: string;
+  TRIB_LPRAZO: string;
+  VL_PATRIM_LIQ: number;
+  AUDITOR: string;
+  CNPJ_AUDITOR: string;
+}
+
 export interface IFunds {
-  [cnpj_fundo: string]: {
-    SIT: string;
-    ADMIN: string;
-    CD_CVM: string;
-    CLASSE: string;
-    CNPJ_ADMIN: string;
-    CONDOM: string;
-    CPF_CNPJ_GESTOR: string;
-    DENOM_SOCIAL: string;
-    DT_CANCEL: string;
-    DT_CONST: string;
-    DT_FIM_EXERC: string;
-    DT_INI_ATIV: string;
-    DT_INI_CLASSE: string;
-    DT_INI_EXERC: string;
-    DT_INI_SIT: string;
-    DT_PATRIM_LIQ: string;
-    FUNDO_COTAS: string;
-    FUNDO_EXCLUSIVO: string;
-    GESTOR: string;
-    INVEST_QUALIF: string;
-    PF_PJ_GESTOR: string;
-    RENTAB_FUNDO: string;
-    TAXA_ADM: number;
-    TAXA_PERFM: number;
-    TP_FUNDO: string;
-    TRIB_LPRAZO: string;
-    VL_PATRIM_LIQ: number;
-    AUDITOR: string;
-    CNPJ_AUDITOR: string;
-  };
+  [cnpj_fundo: string]: IFundAttributes
 }
 
 export interface IUpdate{

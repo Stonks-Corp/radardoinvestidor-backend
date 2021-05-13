24ce259c251f97ed358d37bc2ea55c1864b69493
GiovanniMigon <giovanni.migon@gmail.com>
2021-05-13 17:22:55 -0300

    swagger rodando na localhost/doc, ainda incompleto

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('../swagger_output.json')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
 
 const PORT = 4000;
-
 const app = express();
 
 const corsOptions: cors.CorsOptions = {
   origin: '*',
-  methods: "GET,HEAD,POST",
+  methods: 'GET,HEAD,POST',
 };
 
 app.use(cors(corsOptions));
@@ -19,8 +18,7 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/doc', swaggerUi.serve);
-app.get('/doc', swaggerUi.setup(swaggerFile));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/api', routes);
 
